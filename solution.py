15b4b856178ecd0ee224873b747bc653dafa9599
AshwinK97 <ashwin.kamalakannan@ontariotechu.net>
2020-03-20 22:03:41 -0400

    added report

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ from tensorflow import feature_column
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
+# print numbers formatted
 def printNumbers(path):
     with open(path, 'r') as f:
         reader = csv.reader(f)
@@ -22,7 +23,7 @@ def printNumbers(path):
                     print("")
             print("")
 
-# for each number, generate 50 versions with noise
+# for each number, generate 50 versions with random noise
 def generateDataset():
     with open('./data.csv', 'w') as d:
         with open('./numbers.csv', 'r') as f:
@@ -47,44 +48,51 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=32):
     ds = ds.batch(batch_size)
     return ds
 
+##################################### Main #####################################
+
 # generateDataset()
 # printNumbers("./data.csv")
-
 URL = './data.csv'
 dataframe = pd.read_csv(URL)
 dataframe.head()
 
+# split data into training, validation and testing sets
 train, test = train_test_split(dataframe, test_size=30)
 train, val = train_test_split(train, test_size=50)
 print(len(train), 'train examples')
 print(len(val), 'validation examples')
 print(len(test), 'test examples')
 
+# Choose columns for feature layer
 feature_columns = []
 for header in ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45']:
     feature_columns.append(feature_column.numeric_column(header))
-
 feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
 
+# convert datasets to TF dataset objects
 batch_size = 32
 train_ds = df_to_dataset(train, batch_size=batch_size)
 val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=True, batch_size=batch_size)
 
+# Create keras model
 model = tf.keras.Sequential([
-  feature_layer,
-  layers.Dense(128, activation='relu'),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(1)
+    feature_layer,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+# Compile keras Model
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'],
+)
 
-model.fit(train_ds,
-          validation_data=val_ds,
-          epochs=5)
+# Train the model
+model.fit(train_ds, validation_data=val_ds, epochs=10)
 
+# Test the model
 loss, accuracy = model.evaluate(test_ds)
 print("Accuracy", accuracy)
